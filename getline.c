d6f4e20d532cdffa85abfa302168b3390d0cc172
khawla <boukniter.khawla@gmail.com>
2023-05-24 10:14:42 -0700

    0x16. C - Simple Shell

--- getline.c
+++ getline.c
@@ -1,55 +1,78 @@
 #include "shell.h"
 
 /**
- * mgetline - Read The Input
- * Return: Input
+ * mgetline - read line from the prompt
+ * @info: struct for the program's info
+ * Return: reading counting bytes
  */
-char *mgetline(void)
+int mgetline(datas *info)
 {
-	static char buf[MAX_BUFFER_SIZE];
-	static int indxbuf = 1, buf_size = 1;
-	char curchar = buf[indxbuf++], *l = NULL;
-	int length = 0;
+	char buf[BUFFER_SIZE] = {'\0'};
+	static char *cmds[10] = {NULL};
+	static char oprs[10] = {'\0'};
+	ssize_t bytes, x = 0;
 
-	while (1)
+	if (!cmds[0] || (oprs[0] == '&' && errno != 0) ||
+		(oprs[0] == '|' && errno == 0))
 	{
-		if (indxbuf >= buf_size)
+		for (x = 0; cmds[x]; x++)
 		{
-			buf_size = read(STDIN_FILENO, buf, sizeof(buf));
-			indxbuf = 0;
-			if (buf_size <= 0)
-			{
-				if (length > 0)
-					break;
-				return (NULL);
-			}
-		}
-		if (curchar == '\n')
-			break;
-		l = realloc(l, (length + 1) * sizeof(char));
-		l[length++] = curchar;
+			free(cmds[x]);
+			cmds[x] = NULL;
+		bytes = read(info->filedesc, &buf, BUFFER_SIZE - 1);
+		if (bytes == 0)
+			return (-1);
+		x = 0;
+		do {
+			cmds[x] = str_dup(str_tok(x ? NULL : buf, "\n;"));
+			x = logic_ops(cmds, x, oprs);
+		} while (cmds[x++]);
 	}
-	l = realloc(l, (length + 1) * sizeof(char));
-	l[length] = '\0';
-	return (l);
+	info->linput = cmds[0];
+	for (x = 0; cmds[x]; x++)
+	{
+		cmds[x] = cmds[x + 1];
+		oprs[x] = oprs[x + 1];
+	}
+	return (str_len(info->linput));
 }
 
 /**
- * main - Entry point
- * Return: 0
+ * logic_ops - checks and split for && and ||
+ * @cmds: array of the comd
+ * @x: index to be checked
+ * @oprs: array of the logical operators
+ * Return: index of the last command
  */
-int main(void)
+int logic_ops(char *cmds[], int x, char oprs[])
 {
-	char *inpt = mgetline();
+	char *tmp = NULL;
+	int i;
 
-	printf("#cisfun$ ");
-	fflush(stdout);
-	if (inpt != NULL)
+	for (i = 0; cmds[x] != NULL  && cmds[x][i]; i++)
 	{
-		printf("This is the input: %s\n", inpt);
-		free(inpt);
+		if (cmds[x][i] == '&' && cmds[x][i + 1] == '&')
+		{
+			tmp = cmds[x];
+			cmds[x][i] = '\0';
+			cmds[x] = str_dup(cmds[x]);
+			cmds[x + 1] = str_dup(tmp + i + 2);
+			x++;
+			oprs[x] = '&';
+			free(tmp);
+			i = 0;
+		}
+		if (cmds[x][i] == '|' && cmds[x][i + 1] == '|')
+		{
+			tmp = cmds[x];
+			cmds[x][i] = '\0';
+			cmds[x] = str_dup(cmds[x]);
+			cmds[x + 1] = str_dup(tmp + i + 2);
+			x++;
+			oprs[x] = '|';
+			free(tmp);
+			i = 0;
+		}
 	}
-	else
-		printf("There is no input\n");
-	return (0);
+	return (x);
 }
